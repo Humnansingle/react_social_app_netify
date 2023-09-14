0b37238e0a2670d60613b1a264cdd95ce55e3b55
Humnansingle <bathrirajini4519@gmail.com>
2023-09-14 14:23:06 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+      <h2>About</h2>
+      <p style={{marginTop:"1rem"}}>Hi, i am a  bathri.Together lets make education free 
+      for everyone in the world 
+
+      </p>
+    </main>
+  )
+}
+
+export default About
---
+++ src/Missing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Missing = () => {
+  return (
+    <main className='Missing'>
+      <h1>Page not found</h1>
+      <p>
+        visit our homepage
+      </p>
+    </main>
+  )
+}
+
+export default Missing
